fe7f3bd8f8d587aec0350c711df4e3a3c4503f1d
Diego Leonardo <--global>
2024-07-19 22:06:02 -0300

    inicio do projeto

---
+++ src/main/java/br/com/diego6699/localdorak7/Localdorak7Application.java
@@ -0,0 +1,13 @@
+package br.com.diego6699.localdorak7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Localdorak7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Localdorak7Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diego6699/localdorak7/Localdorak7ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diego6699.localdorak7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Localdorak7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
